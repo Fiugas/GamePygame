b8f323e27d5e8aa04753324d2aa84d5ef013bb25
Fiugas <95915531+Fiugas@users.noreply.github.com>
2024-11-06 18:45:21 +0000

    começo do player outras coisa

--- states/pause_screen.py
+++ states/pause_screen.py
@@ -19,7 +19,7 @@ class PauseScreen(State):
         self.game.reset_player_actions()
 
     def render(self, dt, surface):
-        surface.fill(self.game.colors['BLACK'])
+        surface.blit(self.game.background, (0, 0))
         self.game.draw_text(surface, 'Game paused', self.game.colors['WHITE'], (self.game.GAME_LOGIC_SIZE[0] / 2, self.game.GAME_LOGIC_SIZE[1] / 4))
         self.game.draw_text(surface, 'Press ESC to resume', self.game.colors['WHITE'], (self.game.GAME_LOGIC_SIZE[0] / 2, self.game.GAME_LOGIC_SIZE[1] / 3))
         self.game.draw_text(surface, '1. Exit to menu', self.game.colors['WHITE'], (self.game.GAME_LOGIC_SIZE[0] / 2, self.game.GAME_LOGIC_SIZE[1] / 3 + 20))
