24c6847cba9142db36032de62436098f200149ff
Marycrazy <marianapaisjorgemagalhaes@email.com>
2024-11-20 19:02:00 +0000

    Adiciona lógica de colisão para coleta de chave e saída no jogador

--- components/player.py
+++ components/player.py
@@ -1,25 +1,25 @@
 import pygame
+from components.collision import collision
 class Player:
     def __init__(self, maze, game):
         # Inicializa o player na posição inicial do labirinto
         self.x = maze.start[0]  # Posição X inicial
         self.y = maze.start[1]  # Posição Y inicial
         self.game = game
+        self.collision = collision(maze.key, maze.exit)
 
-    def update(self, dt, player_actions, cell_size):
+
+    def update(self, player_actions, maze):
         # Calcular novos movimentos usando dt para movimento consistente
-        if player_actions['UP']:
-            print("up")
+        if player_actions['UP'] and not maze.is_wall(self.x, self.y - 1):
             self.y -= 1
-        if player_actions['DOWN']:
-            print("down")
+        if player_actions['DOWN'] and not maze.is_wall(self.x, self.y + 1):
             self.y += 1
-        if player_actions['LEFT']:
-            print("left")
+        if player_actions['LEFT'] and not maze.is_wall(self.x - 1, self.y):
             self.x -= 1
-        if player_actions['RIGHT']:
-            print("right")
+        if player_actions['RIGHT'] and not maze.is_wall(self.x + 1, self.y):
             self.x += 1
+        self.collision.check_key_collision(self.x, self.y)
 
     def render(self, surface, cell_size, game):
         self.start_player(surface, cell_size, game)
