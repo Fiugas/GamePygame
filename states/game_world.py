cc836a69abacba7242bbf4a4f48f33ef91a2349f
Marycrazy <marianapaisjorgemagalhaes@email.com>
2024-11-21 13:03:05 +0000

    Adiciona lógica para transição entre níveis e implementa tela de próximo nível

--- states/game_world.py
+++ states/game_world.py
@@ -1,5 +1,6 @@
 from states.state import State
 from states.pause_screen import PauseScreen
+from states.nextlevel_screen import Nextlevelscreen
 from components.maze import Maze
 from components.player import Player
 
@@ -7,7 +8,7 @@ class GameWorld(State):
     def __init__(self, game):
         State.__init__(self, game)
         self.cell_size = 20
-        self.level = 5
+        self.level = 2
         self.maze = Maze(self.level)
         self.player = Player(self.maze, self.game)
 
@@ -16,6 +17,13 @@ class GameWorld(State):
         if player_actions['PAUSE']:
             new_state = PauseScreen(self.game)
             new_state.enter_state()
+        if self.maze.key.collected and self.player.collision.check_exit_collision(self.player.x, self.player.y):
+            new_state = Nextlevelscreen(self.game)
+            self.level += 2
+            self.maze = Maze(self.level)
+            self.player.update_maze(self.maze)
+            new_state.enter_state()
+            print("level: ", self.level)
         self.game.reset_player_actions()
 
     def render(self, dt, surface):
---
+++ states/nextlevel_screen.py
@@ -0,0 +1,26 @@
+from states.state import State
+class Nextlevelscreen(State):
+    def __init__(self, game):
+        State.__init__(self, game)
+        self.menu_options = {0: 'Next level', 1: 'Exit game'}
+        self.menu_cursor = 0
+
+    def update(self, dt, player_actions):
+        self.update_cursor(player_actions)
+        if player_actions['SELECT']:
+            selected_option = self.menu_options[self.menu_cursor]
+            self.handle_selected_option(selected_option)
+        self.game.reset_player_actions()
+
+    def handle_selected_option(self, option):
+        if option == 'Next level':
+            self.exit_state()
+        if option == 'Exit game':
+            self.game.running, self.game.playing = False, False
+        self.game.reset_player_actions()
+
+    def render(self, dt, surface):
+        surface.blit(self.game.background, (0, 0))
+        self.game.draw_text(surface, 'Victory', self.game.colors['WHITE'], (self.game.GAME_LOGIC_SIZE[0] / 2, self.game.GAME_LOGIC_SIZE[1] / 4))
+        self.game.draw_text(surface, 'Next level', self.game.colors['GRAY'] if self.menu_cursor != 0 else self.game.colors['WHITE'], (self.game.GAME_LOGIC_SIZE[0] / 2, self.game.GAME_LOGIC_SIZE[1] / 3 + 40))
+        self.game.draw_text(surface, 'Exit game', self.game.colors['GRAY'] if self.menu_cursor != 2 else self.game.colors['WHITE'], (self.game.GAME_LOGIC_SIZE[0] / 2, self.game.GAME_LOGIC_SIZE[1] / 3 + 80))
