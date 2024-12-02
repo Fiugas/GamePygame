300a58ebcd985deba6254fd75a0ca544e5baba38
Fiugas <95915531+Fiugas@users.noreply.github.com>
2024-12-02 17:13:29 +0000

    teste de fog

--- components/camera.py
+++ components/camera.py
@@ -2,7 +2,7 @@ import pygame
 import math
 
 class Camera:
-    def __init__(self, maze, view_range=3):
+    def __init__(self, maze, view_range=25):
         self.maze = maze
         self.view_range = view_range
         self.fog_surface = None
@@ -17,18 +17,22 @@ class Camera:
         self.offset_y = player_y - center_y
 
     def create_fog_surface(self):
-        max_radius = int(self.view_range * self.maze.cell_size)
+        max_radius = int( 5 * self.maze.cell_size)
         self.fog_surface = pygame.Surface((max_radius * 2, max_radius * 2), pygame.SRCALPHA)
-        self.fog_surface.fill((0, 0, 0, 250))
 
     def apply_fog(self, surface, player_x, player_y):
+        screen_width, screen_height = surface.get_size()
+        self.fog_surface = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
         self.fog_surface.fill((0, 0, 0, 200))
-        max_radius = int(self.view_range * self.maze.cell_size)
+        max_radius = int(5 * self.maze.cell_size)
         fog_clear = pygame.Surface((max_radius * 2, max_radius * 2), pygame.SRCALPHA)
         for r in range(max_radius, 0, -1):
             alpha = int(255 * (r / max_radius) ** 2)
             pygame.draw.circle(fog_clear, (0, 0, 0, 255 - alpha), (max_radius, max_radius), r)
-        clear_rect = fog_clear.get_rect(center=(player_x * self.maze.cell_size, player_y * self.maze.cell_size))
+        player_screen_x = (player_x - self.offset_x) * self.maze.cell_size
+        player_screen_y = (player_y - self.offset_y) * self.maze.cell_size
+
+        clear_rect = fog_clear.get_rect(center=(player_screen_x, player_screen_y))
         self.fog_surface.blit(fog_clear, clear_rect, special_flags=pygame.BLEND_RGBA_SUB)
         surface.blit(self.fog_surface, (0, 0))
 
